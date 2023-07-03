5ac54bd6ee08eab79de700c538a84ad870a545f6
ThanhNam2601 <ltn262001@gmail.com>
2023-07-03 16:34:57 +0700

    consolewriteread

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine ("Summer Time Sadness");
+Console.WriteLine("Nhap ten cua ban: ");
+string ten = Console.ReadLine();
+Console.WriteLine(ten);
